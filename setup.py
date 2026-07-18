2302d887653490987ff358e825596e5abe12477e
Edson Eddy <crew0eddy@gmail.com>
2026-07-18 12:06:48 -0400

    feat: sort similarity groups by average similarity in descending order; increment version to 2.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="csim",
-    version="2.0.3",
+    version="2.0.4",
     packages=find_packages(),
     install_requires=[
         "antlr4-python3-runtime==4.13.2",

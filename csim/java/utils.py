a2e1a1d85b2c9cb51850ce492ee901ac0ed650ff
Edson Eddy <crew0eddy@gmail.com>
2026-08-02 21:12:01 -0400

    feat: add additional excluded rule types for Java20Parser

--- csim/java/utils.py
+++ csim/java/utils.py
@@ -52,12 +52,50 @@ COLLAPSED_RULE_INDICES = {
     Java20Parser.RULE_typeImportOnDemandDeclaration,
     Java20Parser.RULE_singleStaticImportDeclaration,
     Java20Parser.RULE_staticImportOnDemandDeclaration,
-    # Miscellaneous declarations
+    Java20Parser.RULE_packageDeclaration,
+    Java20Parser.RULE_packageModifier,
+    Java20Parser.RULE_importDeclaration,
+    # List wrappers
     Java20Parser.RULE_variableInitializerList,
+    # Name and type wrappers that only restate identifier structure
+    Java20Parser.RULE_typeArguments,
+    Java20Parser.RULE_typeArgumentsOrDiamond,
     # Array dimensions
     Java20Parser.RULE_dims,
     Java20Parser.RULE_dimExpr,
+    Java20Parser.RULE_dimExprs,
+    # Modifier and annotation wrappers
+    Java20Parser.RULE_typeParameterModifier,
+    Java20Parser.RULE_classModifier,
+    Java20Parser.RULE_methodModifier,
+    Java20Parser.RULE_fieldModifier,
+    Java20Parser.RULE_interfaceModifier,
+    Java20Parser.RULE_constructorModifier,
+    Java20Parser.RULE_enumConstantModifier,
+    Java20Parser.RULE_recordComponentModifier,
+    Java20Parser.RULE_constantModifier,
+    Java20Parser.RULE_interfaceMethodModifier,
+    Java20Parser.RULE_annotationInterfaceElementModifier,
+    Java20Parser.RULE_annotation,
+    Java20Parser.RULE_normalAnnotation,
+    Java20Parser.RULE_markerAnnotation,
+    Java20Parser.RULE_singleElementAnnotation,
 }
 HASHED_RULE_INDICES = set()
 CONTROL_EQUIVALENCE_RULE_INDICES = dict()
-EXCLUDED_RULE_TYPES = set()
+EXCLUDED_RULE_TYPES = {
+    # Identifier wrappers: the lexical names are already excluded above,
+    # so keeping these rules only adds noise and extra depth.
+    Java20Parser.RULE_identifier,
+    Java20Parser.RULE_typeIdentifier,
+    Java20Parser.RULE_unqualifiedMethodIdentifier,
+    Java20Parser.RULE_contextualKeyword,
+    Java20Parser.RULE_contextualKeywordMinusForTypeIdentifier,
+    Java20Parser.RULE_contextualKeywordMinusForUnqualifiedMethodIdentifier,
+    Java20Parser.RULE_packageName,
+    Java20Parser.RULE_packageOrTypeName,
+    Java20Parser.RULE_typeName,
+    Java20Parser.RULE_expressionName,
+    Java20Parser.RULE_methodName,
+    Java20Parser.RULE_variableDeclaratorId,
+}

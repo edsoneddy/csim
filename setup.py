79e52472b9c8956e3e45e5812cf2928aaffb60f2
Edson Eddy <crew0eddy@gmail.com>
2026-08-10 20:48:36 -0400

    chore: update version to 3.0.0 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="csim",
-    version="2.0.6",
+    version="3.0.0",
     packages=find_packages(),
     install_requires=[
         "antlr4-python3-runtime==4.13.2",
